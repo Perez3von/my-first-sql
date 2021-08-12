30b1f5804ac5eb8d5b9a112c548d0bca59a3f65b
Evon Perez <perez.evon@gmail.com>
2021-08-11 20:39:39 -0400

    working code

---
+++ data/people.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    name: 'Evon',
+    cool_factor: -100,
+  },
+  {
+    name: 'Justin',
+    cool_factor: 10,
+  },
+  {
+    name: 'Juli',
+    cool_factor: 100,
+  }
+];
